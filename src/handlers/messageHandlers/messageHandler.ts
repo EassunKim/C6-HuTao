cfe64cb3f7a2ff833e72a32a51c5c18425e3362a
Monko2k <kevinhuang2k@gmail.com>
2024-12-01 00:21:13 -0500

    decrease chance for impersonate

--- src/handlers/messageHandlers/messageHandler.ts
+++ src/handlers/messageHandlers/messageHandler.ts
@@ -47,7 +47,7 @@ export const handleMessages = async (client: Client) => {
         }
 
         if (message.mentions.users.size > 0) {
-            proc(1, () => impersonate.execute(message));
+            proc(0.01, () => impersonate.execute(message));
         }
     });
 }
