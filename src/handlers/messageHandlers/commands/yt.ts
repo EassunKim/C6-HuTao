67526c0ed67ca8ce2267cf9928b7d0fcd7f1f8cf
Monko2k <kevinhuang2k@gmail.com>
2024-12-01 22:07:23 -0500

    use proxy instead of cookies

--- src/handlers/messageHandlers/commands/yt.ts
+++ src/handlers/messageHandlers/commands/yt.ts
@@ -2,11 +2,14 @@ import { Message } from "discord.js";
 import { MessageHandler } from "../messageHandler";
 import { AudioPlayerStatus, createAudioPlayer, createAudioResource, DiscordGatewayAdapterCreator, joinVoiceChannel } from "@discordjs/voice";
 import ytdl from '@distube/ytdl-core';
-import { readFileSync } from "fs";
-import { getAuthPath } from "../../../utils/fileUtils";
 
-const execute = async (message: Message, args: string[]) => {
+const execute = async (message: Message, args: string[] = []) => {
     if (!message.member) return;
+    if (args.length != 1) {
+        message.reply('Invalid arguments provided. Please provide a valid URL.');
+        return;
+    }
+    const videoUrl = args[0];
     if (!message.member.voice.channel) {
         message.reply('You need to be in a voice channel to use this command');
         return;
@@ -19,10 +22,15 @@ const execute = async (message: Message, args: string[]) => {
         adapterCreator: voiceChannel.guild.voiceAdapterCreator as DiscordGatewayAdapterCreator,
     });
 
-    const cookiePath = getAuthPath('youtube-cookie.json');
-    const cookies = JSON.parse(readFileSync(cookiePath, "utf-8"));
-    const agent = ytdl.createAgent(cookies);
-    const stream = ytdl(args[0], {filter: 'audioonly', agent});
+    const proxy = process.env.YOUTUBE_PROXY;
+    if (!proxy) {
+        console.error("YouTube Proxy environment variable is not configured");
+        return;
+    }
+
+    const agent = ytdl.createProxyAgent({ uri: proxy });
+
+    const stream = ytdl(videoUrl, {filter: 'audioonly', agent});
     const resource = createAudioResource(stream);
     const player = createAudioPlayer();
 
