90bc04832a240ed84bf4cddef724902e6e70350a
Monko2k <kevinhuang2k@gmail.com>
2024-12-01 20:26:14 -0500

    use cookies for YT

--- src/handlers/messageHandlers/commands/yt.ts
+++ src/handlers/messageHandlers/commands/yt.ts
@@ -2,6 +2,8 @@ import { Message } from "discord.js";
 import { MessageHandler } from "../messageHandler";
 import { AudioPlayerStatus, createAudioPlayer, createAudioResource, DiscordGatewayAdapterCreator, joinVoiceChannel } from "@discordjs/voice";
 import ytdl from '@distube/ytdl-core';
+import { readFileSync } from "fs";
+import { getAuthPath } from "../../../utils/fileUtils";
 
 const execute = async (message: Message, args: string[]) => {
     if (!message.member) return;
@@ -17,9 +19,12 @@ const execute = async (message: Message, args: string[]) => {
         adapterCreator: voiceChannel.guild.voiceAdapterCreator as DiscordGatewayAdapterCreator,
     });
 
-    const player = createAudioPlayer();
-    const stream = ytdl(args[0], { filter: 'audioonly' });
+    const cookiePath = getAuthPath('youtube-cookie.json');
+    const cookies = JSON.parse(readFileSync(cookiePath, "utf-8"));
+    const agent = ytdl.createAgent(cookies);
+    const stream = ytdl(args[0], {filter: 'audioonly', agent});
     const resource = createAudioResource(stream);
+    const player = createAudioPlayer();
 
     player.play(resource);
     connection.subscribe(player);
--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -5,6 +5,10 @@ export const getAssetPath = (assetsLocation: string) => {
     return resolve(__dirname, '../../assets/', assetsLocation);
 }
 
+export const getAuthPath = (authLocation: string) => {
+    return resolve(__dirname, '../../auth/', authLocation);
+}
+
 export const getAssetPaths = (assetsLocation: string) => {
     const filePath = getAssetPath(assetsLocation);
     return readdirSync(filePath).map(fileName => resolve(filePath, fileName));
