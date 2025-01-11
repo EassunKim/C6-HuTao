c72a2a2051d0a0cc675db5a3c41a5bd1b9fde8c7
Monko2k <65096789+Monko2k@users.noreply.github.com>
2025-01-11 01:08:26 -0500

    die

--- src/handlers/messageHandlers/messageHandler.ts
+++ src/handlers/messageHandlers/messageHandler.ts
@@ -75,7 +75,7 @@ export const handleMessages = async (client: Client, openai: OpenAI) => {
         if (message.mentions.users.size > 0) {
             const shouldImpersonate = proc(0.05, () => impersonate.execute(message));
             if (!shouldImpersonate && message.mentions.users.has(USER_KEVIN)) {
-                proc(0.25, () => impersonate.impersonateUser(message, USER_KEVIN));
+                 impersonate.impersonateUser(message, USER_KEVIN);
             }
         }
 
